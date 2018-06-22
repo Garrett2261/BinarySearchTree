079fa0e1f392017fa75507e85a0f9c7d083e5970
Garrett <davisgarrett21@yahoo.com>
2018-06-21 19:53:58 -0500

    Initialized repository

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+        private Node root;
+        
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        
+
+        public void Add(int startValue)
+        {
+            root = new Node(250);
+            
+            
+
+            //Start at the root. Check if the root is null. Assign the root a value. //Then traverse if the root has a value. 
+        }
+
+        public void Search()
+        {
+
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public Node leftChild;
+        public Node rightChild;
+
+        public Node()
+        {
+            leftChild = null;
+            rightChild = null;
+        }
+    }
+}
